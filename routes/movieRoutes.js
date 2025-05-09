9e542dfdd1ea73b990bdeae3ed2db1582b25cff3
walterwrv <walterwrv290888@gmail.com>
2025-05-09 17:57:40 -0300

    Quitar verifyToken en rutas de Movies

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -9,9 +9,9 @@ const router = express.Router();
 // Rutas para el CRUD de movies
 // Rutas protegidas por token
 router.post('/create', verifyToken, checkRole(['admin']), createMovie);
-router.get('/',verifyToken, getMovies);
-router.get('/paginado',verifyToken, getMoviesPaginado);
-router.get('/:id',verifyToken, getMovieById);
+router.get('/', getMovies);
+router.get('/paginado', getMoviesPaginado);
+router.get('/:id', getMovieById);
 router.put('/:id', verifyToken, checkRole(['admin']), updateMovie);
 router.delete('/:id', verifyToken, checkRole(['admin']), deleteMovie);
 
